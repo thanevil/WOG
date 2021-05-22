cf1e94def15287de401e43ad237448542e3e170b
thanevil <thanevil@hotmail.com>
2021-05-22 21:15:10 +0300

    test pipeline

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 import sys
+import os
+
+
+url = os.getenv("URL")
 
 
 def test_scores_service():
-    url = input("what is the current url (ex.http://127.0.0.1:5000/score): ")
+    # url = input("what is the current url (ex.http://127.0.0.1:5000/score): ")
     driver = webdriver.Chrome()
     driver.get(url)
     score = driver.find_element_by_id("score").text
     if 1 < int(score) < 1000:
+        print("test passed!")
         return True
     else:
+        print("test failed")
         return False
 
 
