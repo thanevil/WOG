f912a6ded12d26118bf509306f27449a2ae2ccd1
thanevil <thanevil@hotmail.com>
2021-05-28 13:38:16 +0300

    docker-compose web + mysql mysql volume for persistent data

--- Score.py
+++ Score.py
@@ -4,9 +4,11 @@ import os
 # each win adds 1 to the scores file
 def add_score(difficulty):
     if os.path.exists(
-            '//wsl$/docker-desktop-data/version-pack-data/community/docker/volumes/score-data/_data/Scores.txt'):
+            '//wsl$/docker-desktop-data/version-pack-data/community/docker/volumes/worldofgames_score-data/_data'
+            '/Scores.txt'):
         score = open(
-            '//wsl$/docker-desktop-data/version-pack-data/community/docker/volumes/score-data/_data/Scores.txt', "r+")
+            '//wsl$/docker-desktop-data/version-pack-data/community/docker/volumes/worldofgames_score-data/_data'
+            '/Scores.txt', "r+")
         for num in score:
             score.read()
             new_score = difficulty * 3 + 5 + int(num)
@@ -15,6 +17,7 @@ def add_score(difficulty):
             score.truncate()
     else:
         score = open(
-            '//wsl$/docker-desktop-data/version-pack-data/community/docker/volumes/score-data/_data/Scores.txt', "a")
+            '//wsl$/docker-desktop-data/version-pack-data/community/docker/volumes/worldofgames_score-data/_data'
+            '/Scores.txt', "a")
         score.write(str(int(difficulty) * 3 + 5))
         score.close()
